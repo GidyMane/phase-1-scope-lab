6e3153e5683214c97f25b600e07d0129c561f902
GidyMane <muragegideon2000@gmail.com>
2023-10-03 09:56:09 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// Write your solution in this file!var customerName = "john_doe";
+// Declare a variable in global scope called customerName using var
+var customerName = "bob"
+
+// Write a function that accesses the global customerName variable and uppercases it
+function upperCaseCustomerName() {
+  
+    customerName = customerName.toUpperCase();
+  }
+
+// Write a function that declares a global variable bestCustomer and assigns it to 'not bob'
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+function overwriteBestCustomer(){
+  bestCustomer = "maybe bob"
+}
+const leastFavoriteCustomer = "panda"
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = 'mufasa'
+}
+
+
+
+
+
